860a5d2fb0325df47d77a0f164c22429fee69b4c
Dan Granger <dangranger1907@gmail.com>
2022-02-01 14:20:55 +0000

    added banana bread, commented about banana bread

--- randtext.py
+++ randtext.py
@@ -1,1 +1,2 @@
 print("All we hear is radio gaga, radio goo goo")
+# isn't it wierd that the other file is about banana bread and this one is about queeen, wierd.
--- welcome.py
+++ welcome.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python3
 print("Welcome to my repository")
 last = 1
+running = 1
 for n in range(56):
     running = (n**2*running)/last
     last = n
 print(running, "ooo")
+print("Damn that's a lot of banana bread")
