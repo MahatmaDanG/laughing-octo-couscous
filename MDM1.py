a0d9c1cc8b61b959c773b9ef4ea8d9342f9f0491
Dan Granger <dangranger1907@gmail.com>
2022-02-03 15:33:56 +0000

    Add Generator,__pycache__, adds listing and sorting to MDM1.py

--- MDM1.py
+++ MDM1.py
@@ -1,31 +1,65 @@
 import numpy as np
+import Generator as G
 
 def RandMatGen (N):
     a = np.random.rand(N, N)                            # set diagonal to 0
     np. fill_diagonal(a, 0)                             # np.tril returns a copy of an array with elements above the k-th diagonal zeroed. 
     M = np.tril(a) + np.tril(a, -1).T                   # lower triangle + a transpose of upper triangle
-    M[1,2] = 5
-    print(M)
     return (M)
 
-def MatToList(M,N):
-    List = []
-    X = 0
-    while X < N-1:
-        for Y in range(X+1,N):
-            List.append(M[X,Y])
-        X += 1
-    return(List)
 
 
+def MatToUTList(M,N):                                   # Converts the matrix to and upper triangular list
+    UTList = []
+    X = 0                                               
+    while X < N-1:                                      # Loops through Rows until penultimate (last row isnt in upper triangular) 
 
-#def Matrix_Interpret (M):
+    # Note loop starts from 0 because matrix row 1 is known as 0 in programming  
     
+        for Y in range(X+1,N):                          # Loops through the columns for the upper triangular
+            UTList.append(M[X,Y])                       # appends item to the list
+        X += 1                                          # selects next row
+    return(UTList) 
 
 
-N = 5
+                                     
+
+def CreateTuple(InputList,N):
+    CharacterList = G.MatrixPairingList(N)              # Creates the list of letter pairs
+    TupleList = list(zip(InputList,CharacterList))      # Joins Inputs and letter pairs in ordered pairs
+    return(TupleList)
 
 
-InpMatrix = RandMatGen(N)
-InpList = MatToList(InpMatrix,N)
-print(InpList)
+
+def CombinedListSort(ProcessingList):
+    ProcessingList.sort(key = lambda x: x[0])           # Sorts the combined list by matrix values
+    return(ProcessingList)
+
+    
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~ Temporary Value Assignment ~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+N = 5                                                   # Matrix size (NxN)
+
+
+# ~~~~~~~~~~~~~~~~~
+# ~~ Running Hub ~~
+# ~~~~~~~~~~~~~~~~~
+
+InputMatrix = RandMatGen(N)                             # Makes the input matrix
+InputList = MatToUTList(InputMatrix,N)                  # Turns input matrix into an upper triangular list
+Joined = CreateTuple(InputList, N)                      # Pairs the upper triangular list with letter pairs
+Sorted = CombinedListSort(Joined)                       # Sorts the combined list
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~
+# ~~ Temporary outputs ~~
+# ~~~~~~~~~~~~~~~~~~~~~~~
+
+# print(InputMatrix, "\n")
+# print(InputList, "\n")
+# print(Joined, "\n")
+print (Sorted)
+
+
