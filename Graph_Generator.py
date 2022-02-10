d9df1dd6ffd0a25310f5cf4a2b9220b5de56af93
Vandam Dinh <vandam@eduroam-3-56.nomadic.bris.ac.uk>
2022-02-10 12:25:51 +0000

    Removed unused lines in Graph Gen

--- Graph_Generator.py
+++ Graph_Generator.py
@@ -8,16 +8,6 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 # INPUT - Matrix as a List
-#Matrix = [(0.09518089791960394, 'BD'), (0.1277378312985179, 'AB'), (0.1419332372816624, 'CD'), 
-          (0.533477090542755, 'AD'), (0.6790313116299046, 'AC'), (0.7306722553371253, 'BC')]
-
-#N = int(len(Matrix))
-
-#WeightList = [str(item[0])[:6] for item in Matrix]
-#FromEdgeList = [item[1][0] for item in Matrix]
-#ToEdgeList = [item[1][1] for item in Matrix]
-#EdgeList = list(zip(FromEdgeList[::1],ToEdgeList[::1]))                     # Pairs the edges together e.g. [('B', 'D'), ('A', 'B'),...]
-#WeightDict = {EdgeList[i]: WeightList[i] for i in range(len(EdgeList))}     # Dictionary pairing Edgelist with Weight // Needed for labelling edges
 
 def GenerateGraph(Matrix):
     
