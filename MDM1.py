330d800103d22d0505fcb62e8777eeb443df2f15
Dan Granger <dangranger1907@gmail.com>
2022-02-10 10:39:55 +0000

    Added an output with the correct number of sides, but not necassarily the right ones

--- MDM1.py
+++ MDM1.py
@@ -1,5 +1,7 @@
 import numpy as np
 import Generator as G
+import Graph_Generator as Map
+import math as m
 
 def RandMatGen (N):
     a = np.random.rand(N, N)                            # set diagonal to 0
@@ -35,26 +37,90 @@ def CombinedListSort(ProcessingList):
     ProcessingList.sort(key = lambda x: x[0])           # Sorts the combined list by matrix values
     return(ProcessingList)
 
+def WierdIdea(Sorted):
+    UsedLetters = []                                    # Attempt to avoid loops
+    CorrectPaths = []
+    Rejects = []
+    for index,item in enumerate(Sorted):
+        Count = 0                                       # Counts if the point has already been visited
+        TestLetters = list(item[1])                     # Turns the two letter path into two list elements
+        for letter in UsedLetters:
+            if TestLetters[0] ==  letter:               # Checks if letter one is in list
+                    Count+=1
+            elif TestLetters[1] ==  letter:             # Checks if letter two is in list
+                    Count+=1    
+            print(letter,Count)
+        if Count == 2:
+            Rejects.append(item[1])         # DELETABLE debug check
+            continue
+        CorrectPaths.append(item[1])
+        UsedLetters.extend(TestLetters)
+        UsedLetters = list(set(UsedLetters))
+    return(CorrectPaths)
+
+
+def MinimumSpanningTree(Sorted,N):                        # Sorted: list of paths, order of mag
+    print(Sorted)    
+    UsedLetters = []                                    # Attempt to avoid loops
+    CorrectPaths = []                                   # The programs aim
+    CorrectEven = []
+    CorrectOdd = []
     
-# def MinimumSpanningTree(Sorted):
-#     UsedLetters = []
+    EvenElements = []
+    OddElements = []
+    
+    Rejects = []
+    for index,item in enumerate(Sorted):
+        if index%2 == 0:
+            EvenElements.append(item)
+        else:
+            OddElements.append(item)
+        print (item)                                 
+        Count = 0                                       # Counts if the point has already been visited
+        TestLetters = list(item[1])                     # Turns the two letter path into two list elements
+        print(TestLetters)
+        for letter in UsedLetters:
+            if TestLetters[0] ==  letter:               # Checks if letter one is in list
+                    Count+=1
+            elif TestLetters[1] ==  letter:             # Checks if letter two is in list
+                    Count+=1    
+            print(letter,Count)
+        if Count == 2:
+            Rejects.append(item[1])         # DELETABLE debug check
+            continue
+        CorrectPaths.append(item)
+        if index % 2 == 0:
+            CorrectEven.append(item)
+        else:
+            CorrectOdd.append(item)
+            
+        UsedLetters.extend(TestLetters)
+        UsedLetters = list(set(UsedLetters))
+    EvenCheck = WierdIdea(EvenElements)
+    OddCheck = WierdIdea(OddElements)
+    Length = len(CorrectPaths)
+    if Length < (N-1):
+        print(CorrectPaths,"\n")
+        CorrectPaths.extend(EvenElements[m.ceil((Length/2)):m.ceil((N-1)/2)])
+        CorrectPaths.extend(OddElements[m.floor((Length/2)):m.floor((N-1)/2)])
+    print(CorrectPaths,UsedLetters,Rejects,"\n",CorrectEven,CorrectOdd,"\n", EvenCheck, OddCheck)
+    
+    return(CorrectPaths)
+
+# def MinimumSpanningTree(Sorted,N):
+#     UsedPairs = []
 #     CorrectPaths = []
 #     for item in Sorted:
-#         UsedLetters = list(UsedLetters)                       ####### NOT WORKING #######
-#         Count = 0
-#         TestLetters = list(item[1])
-#         for letter in UsedLetters:
-#             if TestLetters[0] ==  letter:
-#                     Count+=1
-#             elif TestLetters[1] ==  letter:
-#                     Count+=1
-#         if Count == 2:
-#             break
-#         CorrectPaths.append(item[1])
-#         UsedLetters.extend(TestLetters)
-#         UsedLetters = set(UsedLetters)
-#     print(CorrectPaths,UsedLetters)
-                
+        
+        
+
+    
+# def Debug(Sorted):
+#     prints = 0
+#     for item in Sorted:
+#         print(item)  
+#         prints += 1 
+#     print(prints)          
             
         
                     
@@ -74,15 +140,17 @@ InputMatrix = RandMatGen(N)                             # Makes the input matrix
 InputList = MatToUTList(InputMatrix,N)                  # Turns input matrix into an upper triangular list
 Joined = CreateTuple(InputList, N)                      # Pairs the upper triangular list with letter pairs
 Sorted = CombinedListSort(Joined)                       # Sorts the combined list
-# MinimumSpanningTree(Sorted)
+Map.GenerateGraph(MinimumSpanningTree(Sorted,N))
+#Debug(Sorted)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~
 # ~~ Temporary outputs ~~
 # ~~~~~~~~~~~~~~~~~~~~~~~
 
-print(InputMatrix, "\n")
-print(InputList, "\n")
-print(Joined, "\n")
-print (Sorted)
+# print(InputMatrix, "\n")
+# print(InputList, "\n")
+# print(Joined, "\n")
+# print (Sorted)
+# Map.GenerateGraph()
 
 
